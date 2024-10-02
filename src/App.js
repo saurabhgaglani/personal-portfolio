9a9f692500db76eaf9f6e4b8106f05a9b76e520f
saurabhgaglani <saurabhgaglani100@gmail.com>
2024-10-01 18:55:46 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Main from './components/Main';
+import Contact from './components/Contact';
+import Experiences from './components/Experiences';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+
+function App() {
+
+  return (
+      <div className='font-poppins select-none text-black bg-white dark:bg-[#20262E] dark:text-white  transition duration-500'>
+        <Navbar />
+        <Main />
+        <Projects />
+        <Experiences />
+        <Contact />
+      </div>
+  )
+}
+
+export default App
